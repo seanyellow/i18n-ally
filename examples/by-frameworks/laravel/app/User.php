263452e3317e53a9d49339114351f66a7fec2df0
Huang Xingqiao <a92304a92304@gmail.com>
2020-08-18 19:49:00 +0800

    comment

--- examples/by-frameworks/laravel/app/User.php
+++ examples/by-frameworks/laravel/app/User.php
@@ -2,5 +2,5 @@
 
 __('messages.welcome');
 
-__('subfolder/profile.name'); // a valid laravel i18n translation string with slash
-__('subfolder.profile.name'); // it shows inline annotations, but it's an invalid key and it won't work correctly
+__('subfolder/profile.name'); // a valid laravel i18n key with slash
+__('subfolder.profile.name'); // it shows inline annotations after replacing shash with dot, but it's an invalid key and it won't work correctly
